4b1978c35f77b8fe023a5dbf4acc2cc4fe8d65e2
Yao Jie <12301111@bjtu.edu.cn>
2015-07-13 15:32:31 +0800

    teacher一部分代码

--- welearn/src/main/java/com/welearn/controller/teacher/TeacherController.java
+++ welearn/src/main/java/com/welearn/controller/teacher/TeacherController.java
@@ -1,11 +1,17 @@
 package com.welearn.controller.teacher;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.welearn.entity.CourseHomework;
+import com.welearn.entity.Teacher;
 import com.welearn.service.intef.TeacherService;
+import com.welearn.view.View;
 
 @Controller
 @RequestMapping("teacher/teacher/*")
@@ -14,5 +20,38 @@ public class TeacherController {
 	@Resource(name="teacherService")
 	TeacherService teacherService;
 	
+	@RequestMapping("getTeacherById")
+	public String getTeacherById(@RequestParam(value = "teacherId") int teacherId) {
+		
+		int id = teacherId;
+		
+		Teacher teacher = teacherService.getTeacherById(id);
+		System.out.println(teacher.toString());
+		
+		
+		return null;
+	}
+	
+	@RequestMapping("publishHomework")
+	public String getTeacherBy(
+			) {
+		
+		CourseHomework homework = new CourseHomework("作业一", "预习第二章", 1, new Date(), new Date());
+		
+		
+		boolean flag = teacherService.publishCourseHomework(homework);
+		System.out.println(flag);
+		
+		return null;
+	}
+	
+	@RequestMapping("test")
+	public String test(
+			) {
+		
+		teacherService.test();
+		
+		return null;
+	}
 	
 }
--- welearn/src/main/java/com/welearn/service/impl/TeacherServiceImpl.java
+++ welearn/src/main/java/com/welearn/service/impl/TeacherServiceImpl.java
@@ -1,21 +1,118 @@
 package com.welearn.service.impl;
 
+import java.util.Date;
+import java.util.List;
+
+import com.welearn.dao.CourseDao;
+import com.welearn.dao.CourseHomeworkDao;
 import com.welearn.dao.TeacherDao;
+import com.welearn.entity.AttendRecord;
+import com.welearn.entity.AttendTask;
+import com.welearn.entity.Course;
+import com.welearn.entity.CourseHomework;
+import com.welearn.entity.Student;
 import com.welearn.entity.Teacher;
 import com.welearn.service.intef.TeacherService;
 
 public class TeacherServiceImpl implements TeacherService {
 
 	TeacherDao teacherDao;
+	CourseHomeworkDao courseHomeworkDao;
+	CourseDao courseDao;
+
+	public void setCourseDao(CourseDao courseDao) {
+		this.courseDao = courseDao;
+	}
+
 
 	public void setTeacherDao(TeacherDao teacherDao) {
 		this.teacherDao = teacherDao;
+	}
+	
+
+	public void setCourseHomeworkDao(CourseHomeworkDao courseHomeworkDao) {
+		this.courseHomeworkDao = courseHomeworkDao;
 	}
 
 
 	public Teacher getTeacherById(int id) {
 		// TODO Auto-generated method stub
+		//teacherDao.getTeacher(id);
+		return teacherDao.getTeacher(1);
+	}
+
+
+	public boolean publishCourseHomework(CourseHomework homework) {
+		// TODO Auto-generated method stub
+		
+		int result = courseHomeworkDao.addCourseHomework(homework);
+		//System.out.println("result:"+result);
+		return result>0;
+	}
+
+
+	public CourseHomework getHomeworkByTitle(String title) {
+		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	public boolean updateCourseHomework(CourseHomework homework) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public Course getCourseById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Course getCourseByCourseNo(String courseNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public List<Student> getStudentList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public List<Student> getStudentListByCourseId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public List<AttendTask> getAttendTaskByCourseId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public AttendRecord getAttendRecordByAttendTaskId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public boolean publisCourseNotify(String title, String content,
+			int courseId, Date createTime) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public void test() {
+		// TODO Auto-generated method stub
+		List<Course> list = courseDao.getCoursesByAcademyName("软件", 1, 1);
+		System.out.println("-----------------");
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.get(i).toString());
+		}
 	}
 
 }
