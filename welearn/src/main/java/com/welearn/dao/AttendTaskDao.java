d7f1f6208164fc7707ee2bbb33514e622278a3dd
Tracy <lpxiang@bjtu.edu.cn>
2015-07-18 04:09:18 +0800

    学生签到未完成

--- welearn/src/main/java/com/welearn/dao/AttendTaskDao.java
+++ welearn/src/main/java/com/welearn/dao/AttendTaskDao.java
@@ -153,7 +153,7 @@ public class AttendTaskDao extends SuperDao {
 	
 	public List getNowAttendTasks(Date time) {
 		
-		hql = "from AttendTask as a inner join fetch a.courseEntity as c where a.startTime <= ? and a.endTime <= ?";
+		hql = "from AttendTask as a inner join fetch a.courseEntity as c where a.startTime <= ? and a.endTime >= ?";
 		Query query = sessionFactory.getCurrentSession().createQuery(hql);
 		query.setDate(0, time);
 		query.setDate(1, time);
